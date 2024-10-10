c2a0040a7cb94fa338be20b75778c2857a2f5d21
Deng Yufan <ydengbd@connect.ust.hk>
2024-10-10 17:46:02 +0800

    pa1 finish pipe

--- pa1/myshell.c
+++ pa1/myshell.c
@@ -2,8 +2,8 @@
     COMP3511 Fall 2024
     PA1: Simplified Linux Shell (MyShell)
 
-    Your name:
-    Your ITSC email:           @connect.ust.hk
+    Your name: Deng Yufan
+    Your ITSC email: ydengbd@connect.ust.hk
 
     Declaration:
 
@@ -127,6 +127,130 @@ void process_cmd(char *command_line)
     // Uncomment this line to check the cmdline content
     // Please remember to remove this line before the submission
     // printf("Debug: The command line is [%s]\n", command_line);
+    // int pfds[2]; 
+    // pipe(pfds); 
+    // pid_t pid = fork(); /* 0 (child), non-zero (parent) */ 
+    // if ( pid == 0 ) { /* The child process*/ 
+    //     close(1); /* close stdout */ 
+    //     dup2(pfds[1],1); /* make stdout as pipe input */ 
+    //     close(pfds[0]); /* don't need this */ 
+    //     execlp("ls", "ls", NULL); 
+    // } else { /* The parent process*/ 
+    //     wait(0); /* wait for the child process */
+    //     close(0); /* close stdin */ 
+    //     dup2(pfds[0],0); /* make stdin as pipe output */ 
+    //     close(pfds[1]); /* don't need this */
+        
+    //     execlp("wc", "wc", "-l", NULL); 
+    // } 
+    // start by parsing the command line
+    char *pipe_segments[MAX_PIPE_SEGMENTS];
+    int num_pipe_segments;
+    read_tokens(pipe_segments, command_line, &num_pipe_segments, PIPE_CHAR);
+
+    // execute the pipe segments in order
+    for (int i = 0; i < num_pipe_segments; i++)
+    {
+        // print the index and the content of the current pipe segment
+        // printf("Debug: Pipe segment %d: [%s]\n", i, pipe_segments[i]);
+
+        // parse the arguments for the current pipe segment
+        char *arguments[MAX_ARGUMENTS_PER_SEGMENT];
+        // null initialize the arguments array
+        for (int j = 0; j < MAX_ARGUMENTS_PER_SEGMENT; j++)
+        {
+            arguments[j] = NULL;
+        }
+        int num_arguments;
+        read_tokens(arguments, pipe_segments[i], &num_arguments, SPACE_CHARS);
+
+        // if (num_arguments >= MAX_ARGUMENTS_PER_SEGMENT)
+        // {
+        //     arguments[MAX_ARGUMENTS_PER_SEGMENT - 1] = NULL;
+        // }
+
+        if (i != num_pipe_segments - 1)
+        {
+            // open pipe
+            int pipefd[2];
+            pipe(pipefd);
+
+            // create a child process to execute the current pipe segment
+            pid_t pid = fork();
+            if (pid == 0)
+            {
+                // the child process executes the command
+                // if (i != num_pipe_segments - 1)
+                // {
+                // close the read end of the pipe
+                close(1);
+                // redirect the stdout to the write end of the pipe
+                dup2(pipefd[1], 1);
+                // close the write end of the pipe
+                close(pipefd[0]);
+                // }
+                // execute the command
+                execvp(arguments[0], arguments);
+                // if execvp returns, it must have failed
+                // print an error message and exit
+                perror("execvp");
+                exit(1);
+            }
+            else
+            {
+                // the parent process waits for the child to finish
+                wait(0);
+                // if (i != num_pipe_segments - 1)
+                // {
+                // close the write end of the pipe
+                close(0);
+                // redirect the stdin to the read end of the pipe
+                dup2(pipefd[0], 0);
+                // close the read end of the pipe
+                close(pipefd[1]);
+                // }
+            }
+
+
+
+            // num_arguments = num_arguments >= MAX_ARGUMENTS_PER_SEGMENT ? MAX_ARGUMENTS_PER_SEGMENT-1 : num_arguments;
+            // arguments[num_arguments] = NULL;
+
+            // print the arguments for the current pipe segment
+            // for (int j = 0; j < MAX_ARGUMENTS_PER_SEGMENT; j++)
+            // {
+            //     printf("Debug: Argument %d: [%s]\n", j, arguments[j]);
+            // }
+
+            // create a child process to execute the current pipe segment
+            // pid_t pid = fork();
+            // if (pid == 0)
+            // {
+            //     // the child process executes the command
+            //     execvp(arguments[0], arguments);
+            //     // if execvp returns, it must have failed
+            //     // print an error message and exit
+            //     perror("execvp");
+            //     exit(1);
+            // }
+            // else
+            // {
+            //     // the parent process waits for the child to finish
+            //     wait(0);
+            // }
+        }else{
+            execvp(arguments[0], arguments);
+        }
+    }
+
+}
+
+void sigint_handler(int signo)
+{
+    // print the termination message
+    printf(TEMPLATE_MYSHELL_TERMINATE, getpid());
+    // exit the shell
+    exit(1);
 }
 
 /* The main function implementation */
@@ -135,8 +259,11 @@ int main()
     // TODO: replace the shell prompt with your ITSC account name
     // For example, if you ITSC account is cspeter@connect.ust.hk
     // You should replace ITSC with cspeter
-    char *prompt = "ITSC";
+    char *prompt = "ydengbd";
     char command_line[MAX_CMDLINE_LENGTH];
+
+    // register the signal handler for SIGINT
+    signal(SIGINT, sigint_handler);
 
     // TODO:
     // The main function needs to be modified
@@ -151,6 +278,13 @@ int main()
         printf("%s> ", prompt);
         if (get_cmd_line(command_line) == -1)
             continue; /* empty line handling */
+
+        if (strcmp(command_line, "exit") == 0)
+        {
+            // exit the shell
+            printf(TEMPLATE_MYSHELL_END, getpid());
+            exit(0);
+        }
 
         pid_t pid = fork();
         if (pid == 0)
