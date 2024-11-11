2d4552062e8692704519456a37088068b92b19b1
Deng Yufan <ydengbd@connect.ust.hk>
2024-11-11 22:08:50 +0800

    finish pa2

--- pa2/run.sh
+++ pa2/run.sh
@@ -1,2 +1,24 @@
-INDEX='04'
-./mlfq < pa2_given_io/in$INDEX.txt
+# INDEX='01'
+# ./mlfq < pa2_given_io/in$INDEX.txt > pa2_given_io/out$INDEX.txt
+# INDEX='02'
+# ./mlfq < pa2_given_io/in$INDEX.txt > pa2_given_io/out$INDEX.txt
+# INDEX='03'
+# ./mlfq < pa2_given_io/in$INDEX.txt > pa2_given_io/out$INDEX.txt
+# INDEX='04'
+# ./mlfq < pa2_given_io/in$INDEX.txt > pa2_given_io/out$INDEX.txt
+# INDEX='05'
+# ./mlfq < pa2_given_io/in$INDEX.txt > pa2_given_io/out$INDEX.txt
+# INDEX='06'
+# ./mlfq < pa2_given_io/in$INDEX.txt > pa2_given_io/out$INDEX.txt
+# INDEX='07'
+# ./mlfq < pa2_given_io/in$INDEX.txt > pa2_given_io/out$INDEX.txt
+# INDEX='08'
+# ./mlfq < pa2_given_io/in$INDEX.txt > pa2_given_io/out$INDEX.txt
+# INDEX='09'
+# ./mlfq < pa2_given_io/in$INDEX.txt > pa2_given_io/out$INDEX.txt
+# INDEX='10'
+# ./mlfq < pa2_given_io/in$INDEX.txt > pa2_given_io/out$INDEX.txt
+# INDEX='11'
+# ./mlfq < pa2_given_io/in$INDEX.txt > pa2_given_io/out$INDEX.txt
+INDEX='12'
+./mlfq < pa2_given_io/in$INDEX.txt > pa2_given_io/out$INDEX.txt
