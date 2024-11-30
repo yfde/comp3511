fefffb2a7f8cd9cf63e67185e0ce4770c03c408d
Deng Yufan <ydengbd@connect.ust.hk>
2024-11-30 14:32:02 +0800

    finish pa3 v1

---
+++ pa3/run.sh
@@ -0,0 +1,10 @@
+INDEX='1'
+./smm < testcase_given/given$INDEX.txt > testcase_given/out_given$INDEX.txt
+INDEX='2'
+./smm < testcase_given/given$INDEX.txt > testcase_given/out_given$INDEX.txt
+INDEX='3'
+./smm < testcase_given/given$INDEX.txt > testcase_given/out_given$INDEX.txt
+INDEX='4'
+./smm < testcase_given/given$INDEX.txt > testcase_given/out_given$INDEX.txt
+INDEX='5'
+./smm < testcase_given/given$INDEX.txt > testcase_given/out_given$INDEX.txt
